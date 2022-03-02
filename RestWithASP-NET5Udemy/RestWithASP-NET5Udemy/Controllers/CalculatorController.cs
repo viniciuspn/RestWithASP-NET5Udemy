b93edad4d8fa0b18af19016519f934b30526543e
vinicius Nogueira <vinicius@gmail.com>
2022-03-02 11:28:21 -0300

    configuração

--- RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/CalculatorController.cs
+++ RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/CalculatorController.cs
@@ -24,7 +24,7 @@ namespace RestWithASP_NET5Udemy.Controllers
         {
             if (IsNumeric(fristNumber) && IsNumeric(secondNumber))//validar valor numerico
             {
-                var sum = ConvetToDecimal(fristNumber) + ConvetToDecimal(secondNumber);
+                var sum = ConvetToDecimal(fristNumber) + ConvetToDecimal(secondNumber);// calculo soma
                 return Ok(sum.ToString());
             }
             return BadRequest("Invalid Input");
@@ -35,7 +35,7 @@ namespace RestWithASP_NET5Udemy.Controllers
         {
             if (IsNumeric(fristNumber) && IsNumeric(secondNumber))//validar valor numerico
             {
-                var sub = ConvetToDecimal(fristNumber) -  ConvetToDecimal(secondNumber);
+                var sub = ConvetToDecimal(fristNumber) -  ConvetToDecimal(secondNumber);//calculo subtração
                 return Ok(sub.ToString());
             }
             return BadRequest("Invalid Input");
@@ -46,7 +46,7 @@ namespace RestWithASP_NET5Udemy.Controllers
         {
             if (IsNumeric(fristNumber) && IsNumeric(secondNumber))//validar valor numerico
             {
-                var mult = ConvetToDecimal(fristNumber) * ConvetToDecimal(secondNumber);
+                var mult = ConvetToDecimal(fristNumber) * ConvetToDecimal(secondNumber);//calculo multiplicação
                 return Ok(mult.ToString());
             }
             return BadRequest("Invalid Input");
@@ -57,8 +57,30 @@ namespace RestWithASP_NET5Udemy.Controllers
         {
             if (IsNumeric(fristNumber) && IsNumeric(secondNumber))//validar valor numerico
             {
-                var divi = ConvetToDecimal(fristNumber) / ConvetToDecimal(secondNumber);
+                var divi = ConvetToDecimal(fristNumber) / ConvetToDecimal(secondNumber);// calculo divisão
                 return Ok(divi.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("media/{fristNumber}/{secondNumber}")]
+        public IActionResult media(string fristNumber, string secondNumber)
+        {
+            if (IsNumeric(fristNumber) && IsNumeric(secondNumber))//validar valor numerico
+            {
+                var media = ConvetToDecimal(fristNumber) + ConvetToDecimal(secondNumber) / 2;//calculo media
+                return Ok(media.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("raizQuadrada/{fristNumber}")]
+        public IActionResult raizQuadrada(string fristNumber)
+        {
+            if (IsNumeric(fristNumber) )//validar valor numerico
+            {
+                var raizQuadrada = Math.Sqrt((double)ConvetToDecimal(fristNumber));//calculo raiz quadrada
+                return Ok(raizQuadrada.ToString());
             }
             return BadRequest("Invalid Input");
         }
